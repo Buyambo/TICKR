1141eab92a16d181d3daef402eca317eadb1bc5e
Buyambo <50478754+Buyambo@users.noreply.github.com>
2023-03-13 11:24:08 +0200

    Update moving average.py
    Added the ability to let the program decide when to take the trade.

--- moving average.py
+++ moving average.py
@@ -1,14 +1,20 @@
 import yfinance as yf
 
-# get the stock data
-ticker = yf.Ticker("AAPL")
-history = ticker.history(period="max")
-
-# set the moving average period
+# set the stock ticker symbol and moving average period
+ticker_symbol = "AAPL"
 moving_average_period = 50
+
+# get the stock data
+ticker = yf.Ticker(ticker_symbol)
+history = ticker.history(period="max")
 
 # calculate the moving average
 history["Moving Average"] = history["Close"].rolling(window=moving_average_period).mean()
 
-# print the moving average values
-print(history["Moving Average"])
+# determine whether to take a trade based on the moving average
+if history["Close"][-1] > history["Moving Average"][-1] and history["Close"][-2] < history["Moving Average"][-2]:
+    print(f"Buy {ticker_symbol}")
+elif history["Close"][-1] < history["Moving Average"][-1] and history["Close"][-2] > history["Moving Average"][-2]:
+    print(f"Sell {ticker_symbol}")
+else:
+    print(f"No action for {ticker_symbol}")
